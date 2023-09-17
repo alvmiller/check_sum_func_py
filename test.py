7fdba329181a8686924f69134bab77476c5fa62c
alvmiller <134068320+alvmiller@users.noreply.github.com>
2023-09-17 14:35:52 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class SumTestCase(unittest.TestCase):
         """
         Test that sum() will return summary of 2 decimal values
         """
-        self.assertEqual(sum([3, 7]), 11)
+        self.assertEqual(sum([3, 7]), 10)
 
     def test_sum_3_decimals(self):
         # Test 2: summary of 3 decimal values
